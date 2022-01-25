d34fc966dc26da39fda5ce9602fefe97a86c9789
Daylyn Hoxie <dhoxie@eagles.ewu.edu>
2022-01-24 17:13:07 -0800

    Feat: added extension methods to Mixins

--- Logger/BaseLoggerMixins.cs
+++ Logger/BaseLoggerMixins.cs
@@ -2,6 +2,24 @@
 {
     public static class BaseLoggerMixins
     {
-        
+        public static void Error(string message)
+        {
+            // needs to somehow call BaseLogger.Log
+        }
+
+        public static void Warning(string message)
+        {
+
+        }
+
+        public static void Info(string message)
+        {
+
+        }
+
+        public static void Debug(string message)
+        {
+
+        }
     }
 }
